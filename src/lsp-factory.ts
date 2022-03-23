9f16a5e54e2fb102250d624210fcc84e4e93c7dd
Hugo Masclet <hugo@lukso.io>
2022-03-23 13:45:44 +0100

    Add test for URD proxy

--- src/lsp-factory.ts
+++ src/lsp-factory.ts
@@ -10,13 +10,18 @@ const chainId = 22; // Chain Id of the network you want to connect to
 
 const myEOA = web3.eth.accounts.privateKeyToAccount(deployKey);
 
+console.log(`Deploying from: ${myEOA.address}`);
+console.log(
+  `https://blockscout.com/lukso/l14/address/${myEOA.address}/transactions`,
+);
+
 const lspFactory = new LSPFactory(provider, {
   deployKey,
   chainId,
 });
 
 // @ts-ignore
-const main = async () => {
+const testCustomIpfs = async () => {
   // https://docs.lukso.tech/tools/lsp-factoryjs/classes/lsp3-universal-profile#deploy
   const receipt = await lspFactory.LSP3UniversalProfile.deploy(
     {
@@ -40,6 +45,7 @@ const main = async () => {
   console.log(receipt);
 };
 
+// @ts-ignore
 const lsp4 = async () => {
   const receipt = await lspFactory.LSP7DigitalAsset.deploy({
     name: 'My token',
@@ -57,4 +63,32 @@ const lsp4 = async () => {
   );
 };
 
-lsp4();
+const reuseURD = async () => {
+  console.log('Deploy with URD reuse');
+  const receipt = await lspFactory.LSP3UniversalProfile.deploy(
+    {
+      controllerAddresses: ['0xb74a88C43BCf691bd7A851f6603cb1868f6fc147'],
+      lsp3Profile: {
+        name: 'My Universal Profile',
+        description: 'Demo Universal Profile',
+        tags: ['public-profile'],
+        links: [{ title: 'LUKSO Docs', url: 'https://docs.lukso.tech' }],
+      },
+    },
+    {
+      UniversalReceiverDelegate: {
+        deployProxy: true,
+        libAddress: '0x6533158b042775e2FdFeF3cA1a782EFDbB8EB9b1',
+      },
+    },
+  );
+
+  console.log(receipt);
+
+  console.log(
+    // @ts-ignore
+    `You can debug this deployment at: https://erc725-inspect.lukso.tech/inspect?address=${receipt.LSP7DigitalAsset.address}`,
+  );
+};
+
+reuseURD();
